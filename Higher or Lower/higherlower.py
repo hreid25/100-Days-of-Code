0e09913996d728054ad458db942cf90e5d695cad
Hayden <hayd.reid@gmail.com>
2023-01-15 10:44:55 -0500

    Ensure Previous B becomes Next A
    Fix higher or lower game so that previous B becomes next A

--- Higher or Lower/higherlower.py
+++ Higher or Lower/higherlower.py
@@ -4,28 +4,6 @@ import random
 import emoji
 import os
 
-# Iterate through the list of individuals in game data and pass two options to the prompt for the user to choose.
-def present_choices():
-    options = 0
-    for i, thing in enumerate(gamedata.data):
-        dict_num = random.randint(0,49)
-        new_dict = gamedata.data[dict_num]
-        followers = new_dict['follower_count']
-        name = new_dict['name']
-        descr = new_dict['description']
-        country = new_dict['country']
-        options += 1
-        if options == 1:
-            print(f"Compare A: {name}, a {descr} from {country}.")
-            print(higherlowerlogo.versus)
-            a_followers = followers
-        else:
-            print(f"Against B: {name}, a {descr} from {country}.")
-            b_followers = followers
-        if options == 2:
-            break
-    return a_followers, b_followers
-
 # Compare followings of each option
 def compare_followings(option_a_followers,option_b_followers):
     if option_a_followers > option_b_followers:
@@ -34,25 +12,44 @@ def compare_followings(option_a_followers,option_b_followers):
         answer = "B"
     return answer
 
+def generate_account():
+    dict_num = random.randint(0,49)
+    new_dict = gamedata.data[dict_num]
+    return new_dict
+
+def game_continues(account_a,account_b):
+    account_a = account_b
+    account_b = generate_account()
+    # print("game continues!", account_a,account_b)
+    return account_a, account_b
+
 def higher_lower():
     current_score = 0
     game_on = True
     print(higherlowerlogo.logo)
     while game_on is True:
-        # present choices and return follower count from game data
-        a_followers, b_followers = present_choices()
-        # Collect user input on whether option A or B:
-        choice = input("Who has more followers? Type 'A' or 'B':").upper()
-        # Compare user choices follower counts returned from present_choices function
-        answer = compare_followings(a_followers, b_followers)
+        # Grab random accounts
+        if current_score < 1 :
+            account_a = generate_account()
+            account_b = generate_account()
+        # Present choices and return follower count from game data
+        print(f"Compare A: {account_a['name']}, a {account_a['description']} from {account_a['country']}.")
+        print(higherlowerlogo.versus)
+        print(f"Against B: {account_b['name']}, a {account_b['description']} from {account_b['country']}.")
+        choice = input("\nWho has more followers? Type 'A' or 'B': ").upper()
+        # Compare follower count of each account
+        answer = compare_followings(account_a['follower_count'], account_b['follower_count'])
         if choice == answer:
-            os.system('cls||clear')
+            game_on = True
             current_score +=1
+            os.system('cls||clear')
             print(higherlowerlogo.logo)
             print(f"CORRECT! Your current score is {current_score}!! ",emoji.emojize(':thumbs_up:'))
-            # Some code to make next comparison A == answer[index]
+            account_a, account_b = game_continues(account_a, account_b)
         else:
-            print(f"Sorry, that's wrong. Final score: {current_score}")
             game_on = False
+            print(f"Sorry, that's wrong. Final score: {current_score}")
             break
+        
 higher_lower()
+
