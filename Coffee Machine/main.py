e640b4be7490d340255cfd91699881bf351f28ed
Hayden <hayd.reid@gmail.com>
2023-01-17 22:01:27 -0500

    Refactor and Clean Up
    Ensure functions working as expected, refactor code, ensure deductions against resources properly made, check that resources sufficient working correctly, add to do items

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -16,22 +16,18 @@ def print_report():
 # def turn_off_machine():
 
 def check_resources_sufficient(item_ordered):
-    # deduct resources from existing ingredients in menu
-    for key, value in menu.MENU.items():
-        if item_ordered == key:
-            # Collect total water, milk and coffee cost of drink
-            for key, value in menu.MENU[item_ordered]['ingredients'].items():
-                for totals_key, totals_value in menu.resources.items():
-                    if key == totals_key:
-                        if value < totals_value:
-                            # print("latte cost: ", value, "resources: ", totals_value)
-                            resources_sufficient = True
-                        elif value >= totals_value:
-                            # print("latte cost: ", value, "resources: ", totals_value)
-                            resources_sufficient = False
+    # Check only the resources required to make the drink are sufficient
+    for totals_key, totals_value in menu.resources.items():
+        drink_resource_cost = menu.MENU[item_ordered]['ingredients'][totals_key]
+        if float(drink_resource_cost) < float(totals_value):
+            # print("drink ", totals_key, " cost: ", drink_resource_cost, " available ", totals_key, " cost: ", totals_value)
+            resources_sufficient = True
+        elif float(drink_resource_cost) > float(totals_value):
+            # print("drink ", totals_key, " cost: ", drink_resource_cost, " available ", totals_key, " cost: ", totals_value)
+            resources_sufficient = False
+            print(f"Sorry, there isn't enough {totals_key}.")
+            break
     return resources_sufficient
-            # for key, value in
-            # Subtract the water, milk and coffee total from the resources dict
 
 def process_coins():
     coins = {'quaters':0.25, 'dimes':0.1, 'nickels':.05, 'pennies':0.01}
@@ -39,60 +35,70 @@ def process_coins():
     for key,value in coins.items():
         coin_quantity = float(input(f"How many {key}?: "))
         coins_inserted += (coin_quantity * value)
-    print(coins_inserted)
+    # print(coins_inserted)
     return coins_inserted
 
 # Asks to insert coins and quantity of each type of coin
 def check_transaction(coins_inserted,drink_cost):
     # Check transaction successful
     if coins_inserted < drink_cost:
-        print("Sorry that's not enough money. Money refunded.")
-    elif coins_inserted > drink_cost:
-        # Return change
+        enough_money = False
+        change_to_give = 0
+    elif coins_inserted >= drink_cost:
+        enough_money = True
         change_to_give = (coins_inserted - drink_cost)
-    return change_to_give
+    return enough_money, change_to_give
 
 # Get drink cost
 def determine_item_cost(item_ordered):
     for key, value in menu.MENU.items():
         if item_ordered == key:
             drink_cost = menu.MENU[key]['cost']
-    print(key,drink_cost)
+    # print(key,drink_cost)
     return drink_cost
 
 def make_coffee(item_ordered, drink_cost,change_to_give):
     # Insert money to coffers
-    menu.resources['Money'] += drink_cost
+    if 'Money' in menu.resources:
+        menu.resources['Money'] += float(drink_cost)
+    else:
+        menu.resources['Money'] = float(drink_cost)
     # Deduct drink resources required from total resources
-    for key, value in menu.resources.items():
-        if key == item_ordered:
-            key[value] -= menu.MENU[key]['ingredient']
+    for key, value in menu.MENU[item_ordered]['ingredients'].items():
+        # print("current resources : " ,key, menu.resources[key], "subtract", value)
+        menu.resources[key] -= value
     # Show the user that their order has been completed and present them an emoji :)
     print(f"Here is $ {change_to_give} in change.")
-    print(f"Here is your {item_ordered} ", emoji.emojize(":hot_beverage:"))
+    print(f"Here is your {item_ordered}. Enjoy! ", emoji.emojize(":hot_beverage:"))
 
 def main():
     order_processing = True
     while order_processing == True:
-        item_ordered = input("What would you like? (espresso/latte/cappucino)")
+        item_ordered = input("What would you like? (espresso/latte/cappuccino)")
         # Print a report of the coffee machines resources when user types 'report'
-        if item_ordered == "Report":
+        if item_ordered.lower() == "report":
             print(menu.resources)
-        # determine drink cost
-        drink_cost = determine_item_cost(item_ordered)
-        # add total value of coins inserted
-        coins_inserted = process_coins()
-        # Check that the coins inserted is at least equal to the cost of the drink and return change if necessary.
-        change_to_give = check_transaction(coins_inserted, drink_cost)
-        if change_to_give is None:
-            order_processing = False
+        elif item_ordered.lower() == "off":
+            order_processing == False
             break
-        else:
+        elif item_ordered == "espresso" or "latte" or "cappuccino":
             resources_sufficient = check_resources_sufficient(item_ordered)
             if resources_sufficient == True:
-                make_coffee(item_ordered, drink_cost,change_to_give)
+                # determine drink cost
+                drink_cost = determine_item_cost(item_ordered)
+                # add total value of coins inserted
+                coins_inserted = process_coins()
+                # Check that the coins inserted is at least equal to the cost of the drink and return change if necessary.
+                enough_money, change_to_give = check_transaction(coins_inserted, drink_cost)
+                if enough_money == True:
+                    make_coffee(item_ordered, drink_cost,change_to_give)
+                else:
+                    print("Sorry that's not enough money. Money refunded.")
+                    order_processing = False
             else:
                 order_processing = False
                 break
+        else:
+            print("That item is not on the menu. Please enter a valid option.")
 
 main()
