617ffb18f65dd5d5ce70484d97ffae998a933b8c
Hayden <hayd.reid@gmail.com>
2023-01-20 17:52:05 -0500

    Getting Turtle to Create Shapes
    Added loop to have turtle create shapes

--- Turtle Challenge/main.py
+++ Turtle Challenge/main.py
@@ -1,23 +1,28 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-# tim.shape("turtle")
+tim.shape("turtle")
 tim.color("blue")
-# timmy.setpos(60,30)
-end_of_square = tim.position()
+# end_of_square = tim.position()
 
 # for _ in range(4):
-#     tim.forward(100)
-#     tim.right(90)
+    # tim.forward(100)
+    # tim.right(angle)
 
-for _ in range(30):
-    tim.forward(10)
-    tim.pendown()
-    tim.forward(10)
-    tim.penup()
+sides = [{'triangle':3, 'square':4, 'pentagon':5, 'hexagon':6, 'heptagon':7, 'octagon':8, 'nonagon':9, 'decagon':9}]
+angles = {k:360/v for k,v in sides[0].items()}
 
+for shape, angle in angles.items():
+    print("shape: ", shape, "| angle: ", angle)
+    for turn in sides[0]:
+        tim.forward(100)
+        tim.right(angle)
 
-
+# for _ in range(30):
+#     tim.forward(10)
+#     tim.pendown()
+#     tim.forward(10)
+#     tim.penup()
 
 screen = Screen()
 screen.exitonclick()
