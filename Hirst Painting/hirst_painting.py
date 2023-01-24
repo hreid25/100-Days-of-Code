39ca9f00e8fb9ed13996a52d93511538372ef488
Hayden <hayd.reid@gmail.com>
2023-01-23 22:31:28 -0500

    Draw Hirst Painting
    Testing dots, screensize, setting position

--- Hirst Painting/hirst_painting.py
+++ Hirst Painting/hirst_painting.py
@@ -1,12 +1,42 @@
 import os
 import colorgram
-# be able to change the size of the dot on the screen
-# extract colors from the image using colorgram
+from turtle import Turtle, Screen
+import random
 
-colors = colorgram.extract('C://Users//haydr//100 Days of Code///Hirst Painting//damien_hirst.jpg', 10)
+# reset the turtles position once it reaches the edge of the window
+# Move tim up 50 from the leftmost dot from the opposite edge and repeat the dotting process.
 
-rgb_list = [i.rgb for i in colors]
-lst_of_tuples = []
-for value in rgb_list:
-    lst_of_tuples.append(tuple((rgb_list[0].r,rgb_list[0].g,rgb_list[0].b)))
-print(lst_of_tuples)
+tim = Turtle()
+tim.shape("turtle")
+# tim.color("coral")
+screen = Screen()
+screen.colormode(255)
+screen.screensize(100, 100)
+
+color_list = [(249, 248, 248), (238, 246, 243), (246, 240, 244), (235, 241, 246), (1, 13, 31), (52, 25, 17), (219, 127, 106), (9, 105, 160), (242, 214, 69), (150, 84, 39)]
+
+def return_rgb():
+    color = random.choice(color_list)
+    print(color)
+    return color 
+
+for i in range(100):
+    if tim.pos() == (850.00,0.00):
+        tim.setpos(0.00,50.00)
+    print(tim.pos(), i)
+    tim.dot(20)
+    tim.penup()
+    tim.pencolor(return_rgb())
+    tim.forward(50)
+    tim.pendown()
+
+screen.exitonclick()
+
+
+# colors = colorgram.extract('C://Users//haydr//100 Days of Code///Hirst Painting//damien_hirst.jpg', 10)
+
+# rgb_list = [i.rgb for i in colors]
+# color_list = []
+# for i, value in enumerate(rgb_list):
+#     color_list.append(tuple((rgb_list[i].r,rgb_list[i].g,rgb_list[i].b)))
+# print(color_list)
