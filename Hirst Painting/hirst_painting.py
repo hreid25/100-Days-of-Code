80e0ac5f09713d1f7a9869b7e56c091ceb573c42
Hayden <hayd.reid@gmail.com>
2023-01-24 11:03:36 -0500

    Complete Hirst Painting

--- Hirst Painting/hirst_painting.py
+++ Hirst Painting/hirst_painting.py
@@ -5,14 +5,13 @@ import random
 
 tim = Turtle()
 tim.shape("turtle")
-# tim.color("coral")
 screen = Screen()
 screen.colormode(255)
+
 canvasheight = 10
 canvaswidth = 10
 screen.screensize(canvasheight,canvaswidth)
 
-print(type(screen.screensize(canvasheight,canvaswidth)))
 color_list = [(249, 248, 248), (238, 246, 243), (246, 240, 244), (235, 241, 246), (1, 13, 31), (52, 25, 17), (219, 127, 106), (9, 105, 160), (242, 214, 69), (150, 84, 39)]
 
 def return_rgb():
@@ -27,18 +26,22 @@ def move_y_coordinates(current_y):
     y_coord = (0.00,current_y)
     return y_coord
 
-# start_pos = tim.setpos(10,0)
+tim.hideturtle()
 print(screen.window_width(),screen.window_height())
+dot_drawn = 0
+tim.speed("fastest")
 for i in range(100):
-    # print(tim.pos()[0],screen.canvwidth())
-    if tim.pos()[1] > screen.screensize():
+    if dot_drawn == 10:
         tim.penup()
         tim.setpos(move_y_coordinates(tim.pos()[1]))
+        dot_drawn = 0
     tim.dot(20)
+    dot_drawn += 1
     tim.penup()
     tim.pencolor(return_rgb())
     tim.forward(50)
     tim.pendown()
+
 
 screen.exitonclick()
 
