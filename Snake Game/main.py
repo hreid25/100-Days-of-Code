7d651cbd0a9c642fb88f882978b71e93e04cc198
Hayden <hayd.reid@gmail.com>
2023-01-30 08:56:19 -0500

    Complete Scoreboard
    Add database checks, complete scoreboard visuals

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -2,12 +2,17 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
-import scoreboard
+from scoreboard import Scoreboard
+import sqlite3
+import os
 
 # Create a title screen and a "Start Game" & "End" option
 # grow the snake each time it eats food
 # randomize the location of the food
 # If the snake hits itself or the edge of the screen, the snake dies and the game restarts
+path = os.getcwd()
+print(path)
+os.chdir(path + "//Snake Game")
 
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -18,6 +23,14 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
+
+conn = sqlite3.connect('snakescores.db')
+# Check existence of userdata table
+if not scoreboard.check_table_exists(conn, ('scoredata',)):
+    scoreboard.create_table()
+conn.commit()
+conn.close()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -33,6 +46,7 @@ while game_on:
     # Detect collision with Food
     if snake.head.distance(food) < 16.5:
         food.touched_food()
+        scoreboard.user_scored()
         # print("found the food!")
         # recreate the food in another location and destroy the last piece of food
     # Add a piece to the snake after eating
--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,19 +1,24 @@
 import sqlite3
+from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ('Arial',20,'normal')
 
-class Scoreboard():
+class Scoreboard(Turtle):
     def __init__(self):
+        super().__init__()
         self.score = 0
-        self.user = ""
-        # initialize the database connection
-        if self.check_table_exists():
-            print("table created")
-        else:
-            self.create_table()
+        # self.user = ""
+        self.penup()
+        self.color("white")
+        self.goto(0,270)
+        self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
+        self.hideturtle()
 
-    
     def user_scored(self):
-        pass
-    # add a number to the users score
+        # increase score
+        self.score += 100
+        self.clear()
+        self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
 
     def create_table(self):
         conn = sqlite3.connect('snakescores.db')
@@ -21,23 +26,26 @@ class Scoreboard():
         c.execute("""CREATE TABLE scoredata (
             record_id INTEGER NOT NULL PRIMARY KEY,
             username text,
-            score
+            score,
             UNIQUE(username)
             )""")
         conn.commit()
         conn.close()
 
-    def check_table_exists(dbcon, tablename):
+    def check_table_exists(self,dbcon,table_name):
+    # Function takes in a tuple containing the variable name you wish to pass into the SQL statement
         dbcur = dbcon.cursor()
-        dbcur.execute(f"""
-        SELECT COUNT(*)
-        FROM information_schema.tables
-        WHERE table_name = '{tablename}'
-        """)
-        if dbcur.fetchone()[0] == 1:
-            dbcur.close()
-            return True
-        dbcur.close()
-        return False
-    
+        dbcur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name=?", table_name)
+        if dbcur.fetchone()[0] == 1 :
+            table_exist = True
+            print(f"{table_name} exists")
+        else:
+            table_exist = False
+            print(f"{table_name} does not exist")
+        return table_exist
+
+    def game_ended(self):
+        # compare the latest score with the score from the DB
+        # if score is higher, update the DB score
+        pass
 
