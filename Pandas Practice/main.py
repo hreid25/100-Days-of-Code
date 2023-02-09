52ae2ecfe4a48cb4b33e0e6c5b67e37d6333d085
Hayden <hayd.reid@gmail.com>
2023-02-09 09:09:38 -0500

    More Practice

--- Pandas Practice/main.py
+++ Pandas Practice/main.py
@@ -10,4 +10,33 @@ import numpy as np
 #         temperatures.append(int(row[1]))
 
 data = pd.read_csv('weather_data.csv')
-print(data["temp"])
+temperatures = data["temp"].to_list()
+
+# print(data[data["day"] == "Monday"])
+# print(data[data["temp"] == data["temp"].max()])
+
+def convert_to_f(celsius):
+    fahrenheit = (celsius * (9/5) + 32)
+    return fahrenheit
+
+monday = data[data.day == "Monday"]
+celsius = int(monday.temp)
+fahrenheit = convert_to_f(celsius)
+print(fahrenheit)
+
+# Create dataframe from scratch
+data_dict = {
+    "students" : ['Amy','John','Stuart'],
+    "scores" : [22,88,95]
+}
+
+data = pd.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
+# print(sum(temperatures))
+
+# sum_temp = sum(temperatures)
+# avg = sum_temp / len(temperatures)
+
+# print(avg)
+# for temp in temperatures:
