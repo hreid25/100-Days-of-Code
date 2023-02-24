0eb9d18856c0c09847c05d4faa1a871a07819a83
Hayden <hayd.reid@gmail.com>
2023-02-23 21:46:54 -0500

    Configure Timer

--- Pomodoro App/main.py
+++ Pomodoro App/main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from datetime import datetime, timedelta
-import time
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -12,31 +11,49 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def timer_restart():
-    pass
+    canvas.delete('default')
+    canvas.delete('b_type')
+    timer_text = canvas.create_text(102,130,text="00:00", font=(FONT_NAME, "20", "bold"),fill='white',tag='default')
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def timer_start():
+    global REPS
+    REPS += 1
     # set current time btn was clicked
+    b_type = canvas.create_text(102,100,text='Work Timer',font=(FONT_NAME,'20','bold'), fill='white',tag='b_type')
     time_start = datetime.now()
     # set the time at which the timer should end
-    end_timer = time_start + timedelta(minutes=25)
-    timer = end_timer - datetime.now()
-    count_down(time_start,end_timer,timer)
-    # # start printing the countdown on the tomato
-    # while end_timer != time_start:
-    #     time.sleep(1)
-    #     
-    
+    work_end = time_start + timedelta(minutes=25)
+    work_timer = work_end - datetime.now()
+    # set the short break timer
+    sb_end = time_start + timedelta(minutes=5)
+    sb_timer = sb_end - datetime.now()
+    # set the long break timer
+    lb_end = time_start + timedelta(minutes=25)
+    lb_timer = lb_end - datetime.now()
+    # call the count down function according to the reps
+    # short break timer occurs 2/4/6
+    if REPS % 2 == 0:
+        canvas.itemconfig(b_type, text='Short Break')
+        count_down(sb_timer)
+    # long break timer occurs at 8 reps
+    elif REPS % 8 == 0:
+        canvas.itemconfig(b_type, text='Long Break')
+        count_down(lb_timer)
+    # work timer occurs 1/3/5/7 rep
+    else:
+        canvas.itemconfig(b_type, text='Work Timer')
+        count_down(work_timer)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(time_start,end_timer,timer):
+# # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(timer):
     canvas.itemconfig(timer_text, text=timer)
-    print(type(timer))
-    while end_timer != time_start:
-        window.after(1000, count_down, timer - timedelta(seconds=1))
+    window.after(1000, count_down, timer - timedelta(seconds=1))
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -58,11 +75,14 @@ timer_label.grid(column=2,row=1)
 start_btn = Button(text="Start",font=(FONT_NAME,'10','bold'),command=timer_start)
 start_btn.grid(column=1,row=3)
 
-reset_btn = Button(text="Reset",font=(FONT_NAME,'10','bold'))
+reset_btn = Button(text="Reset",font=(FONT_NAME,'10','bold'),command=timer_restart)
 reset_btn.grid(column=3,row=3)
 
 check_marks = Label(text="✓",bg=GREEN,fg=RED)
 check_marks.grid(column=2,row=3)
 
-# timer_start()
+
+
+
+
 window.mainloop()
